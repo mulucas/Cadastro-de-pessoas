8a7360b81f73195273dd824258918563e900b904
lucas <lukasqueiroz54@gmail.com>
2020-09-11 08:20:57 -0300

    Spring Security e Validação dos Usuários

---
+++ src/main/java/curso/springboot/security/WebConfigSecurity.java
@@ -0,0 +1,41 @@
+package curso.springboot.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
+	
+	@Override //configura as solicitacoes de acesso por http
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.GET, "/").permitAll()
+		.anyRequest().authenticated().and().formLogin().permitAll().and().logout()
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(implementacaoUserDetailsService)
+		.passwordEncoder(new BCryptPasswordEncoder());
+		
+//		auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
+//		.withUser("lucas").password("123").roles("ADMIN");
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/materialize/**");
+	}
+}
